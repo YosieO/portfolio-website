c51d0d8975b79d3d556e2c82498fabbcaa9726f9
YosieO <yosie.octavia@gmail.com>
2022-01-28 23:19:15 +0100

    Use linters to validate

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,10 @@
 console.log('Welcome to my portfolio! 😃')
 
-function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-        x.className += ' responsive';
-    } else {
-        x.className = 'navtoggle';
-    }
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
 }
-
-$(window).scroll(function() {
-    if ($(this).scrollTop()> 20) {
-        $('#arrow').fadeOut();
-     }
-    else {
-      $('#arrow').fadeIn();
-     }
- });
